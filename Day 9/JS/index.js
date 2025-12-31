5bcf4ba1211cc699e0422ac1562d6aaaa107d47e
adimore96 <91413755+adimore96@users.noreply.github.com>
2025-12-31 10:27:55 +0530

    Variable and Function Scope and Hoisting

---
+++ Day 9/JS/index.js
@@ -0,0 +1,18 @@
+function A(){
+    var a = 10;
+    var b = 20;
+    console.log(`Sum: ${a+b}`); 
+}
+
+function B(){
+     x = 5;
+    var y = 15;
+    console.log(`Sub: ${x-y}`); 
+}
+
+//console.log(x); // ReferenceError: x is not defined
+A();
+B()
+console.log(x); // 5
+
+
